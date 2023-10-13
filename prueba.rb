5c20564878d8c3f4d6c555d9ff518568496ed469
JackNegro <jackcuba01@gmail.com>
2023-10-13 01:46:55 -0400

    prueba ruby

---
+++ prueba.rb
@@ -0,0 +1,23 @@
+=begin
+esto es 
+un comentario
+multilinea
+=end
+puts"Hola mundo"
+#comentario lineal
+puts "adios mundo"
+puts""
+#numeros
+puts "numeros"
+puts 5+5
+puts 3/2.0
+puts""
+#variables
+puts"variables"
+numf = 4
+num = 2
+#string
+cad = "keyller"
+cad2="chris"
+puts cad +"\s"+ cad2
+puts 'hola '+cad
